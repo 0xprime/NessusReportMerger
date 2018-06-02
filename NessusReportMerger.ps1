b23487825b4c386a6bb9739dbf657e516fdc285f
0xPrime <eivind.utnes@gmail.com>
2018-06-03 01:55:48 +0200

    Initial version

---
+++ NessusReportMerger.ps1
@@ -0,0 +1,71 @@
+﻿<#
+.SYNOPSIS
+Merges multiple .nessus files into one file. 
+
+Inspired by: 
+https://gist.github.com/mastahyeti/2720173 by Ben Toews
+https://www.darkoperator.com/blog/2015/2/16/merging-nessus-xml-reports-with-powershell by Carlos Perez
+By 0xPrime
+.DESCRIPTION
+Merges multiple .nessus files into one file. 
+
+#>
+
+Param
+(
+    # Path to the files. Defaults to the current path. 
+    [string] $filepath = (Resolve-Path .\).Path,
+    # Output file name. Defauilts to merged_nessus_report.nessus.
+    [string] $outputfilename = "merged_nessus_report.nessus",
+    # Name of the merged report. Defaults to Merged Report.
+    [string] $reportName = "Merged Report"
+)
+
+Write-Host Merging .nessus files in folder $filepath 
+
+# Get all .nessus files
+$files = Get-ChildItem $filepath -Filter *.nessus
+
+# Check if the folder contains .nessus files
+if ($files.count -eq 0) 
+{
+    Write-Host No .nessus files detected in $filepath -f Red
+}
+else
+{
+    foreach ($file in $files)
+    {
+        # Check if this is the first file
+        if ($files.IndexOf($file) -eq 0)
+        {
+            Write-Host Selected $file as primary -f Green
+            Write-Host Parsing $file -f Green
+            [xml]$mergedReport = Get-Content -Path $file
+        }
+        else
+        {
+            Write-Host Parsing $file -f Green
+            
+            # Get all the ReportHost nodes from the XML
+            [xml]$report = Get-Content -Path $file
+            $reportHosts = $Report.NessusClientData_v2.Report.SelectNodes("ReportHost")
+            
+            foreach($reportHost in $reportHosts)
+            {
+                Write-Host + Adding host: $reportHost.Name  -f Green
+
+                # Merge each host into the merged report. 
+                $node = $mergedReport.ImportNode($reportHost, $true)
+                $mergedReport.NessusClientData_v2.Report.AppendChild($node)
+            }
+        }    
+    }
+    # Set the report name.
+    $mergedReport.NessusClientData_v2.Report.name = $reportName
+    $outfile = $filepath + "\" + $outputfilename
+
+    Write-Host Saving merged report to $outfile  -f Green
+    
+    # Save the merged report to a file. 
+    $mergedReport.Save($outfile)
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Merges multiple .nessus files into one file.
+
+Inspired by:
+https://gist.github.com/mastahyeti/2720173 by Ben Toews
+https://www.darkoperator.com/blog/2015/2/16/merging-nessus-xml-reports-with-powershell by Carlos Perez
