fde75dd86faeefc64e076a87eee4325d25a28f21
0xprime <eivind.utnes@gmail.com>
2018-06-03 02:04:09 +0200

    Changed synopsis.

--- NessusReportMerger.ps1
+++ NessusReportMerger.ps1
@@ -2,13 +2,12 @@
 .SYNOPSIS
 Merges multiple .nessus files into one file. 
 
+.DESCRIPTION
+Merges multiple .nessus files into one file. 
 Inspired by: 
 https://gist.github.com/mastahyeti/2720173 by Ben Toews
 https://www.darkoperator.com/blog/2015/2/16/merging-nessus-xml-reports-with-powershell by Carlos Perez
 By 0xPrime
-.DESCRIPTION
-Merges multiple .nessus files into one file. 
-
 #>
 
 Param
