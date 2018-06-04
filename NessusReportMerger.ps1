7d9a48346fd65c03be9f72a079e01c0daaea727c
0xPrime <eivind.utnes@gmail.com>
2018-06-04 23:27:15 +0200

    Added examples.

--- NessusReportMerger.ps1
+++ NessusReportMerger.ps1
@@ -3,11 +3,31 @@
 Merges multiple .nessus files into one file. 
 
 .DESCRIPTION
-Merges multiple .nessus files into one file. 
+Merges multiple .nessus files into one file.  The resulting file can be imported into Nessus again to genereate a single report. 
+
 Inspired by: 
 https://gist.github.com/mastahyeti/2720173 by Ben Toews
 https://www.darkoperator.com/blog/2015/2/16/merging-nessus-xml-reports-with-powershell by Carlos Perez
 By 0xPrime
+
+.EXAMPLE
+NessusReportMerger.ps1 
+Runs with default variables for -filepath, -outputfilename and -reportname.
+Merges the files in the current folder to the file "merged_nessus_report.nessus" and sets the report name to "Merged Report".
+
+.EXAMPLE
+NessusReportMerger.ps1 -filepath "C:\foo"
+Runs with default variables for -outputfilename and -reportname. 
+Merges the files in "C:\foo" to the file "merged_nessus_report.nessus" and sets the report name to "Merged Report".
+
+.EXAMPLE
+NessusReportMerger.ps1 -reportname "FooReport"
+Runs with default variables for -filepath and -outputfilename.
+Merges the files in the current folder to the file "merged_nessus_report.nessus" and sets the report name to "FooReport".
+
+.EXAMPLE
+NessusReportMerger.ps1 -filepath "C:\foo" -outputfilename "FooReport.nessus" -reportname "FooReport"
+Merges the files in the "C:\foo" folder to the file "FooReport.nessus" and sets the report name to "FooReport".
 #>
 
 Param
@@ -62,7 +82,7 @@ else
     $mergedReport.NessusClientData_v2.Report.name = $reportName
     $outfile = $filepath + "\" + $outputfilename
 
-    Write-Host Saving merged report to $outfile  -f Green
+    Write-Host Saving merged report to $outfile  -f Green 
     
     # Save the merged report to a file. 
     $mergedReport.Save($outfile)
